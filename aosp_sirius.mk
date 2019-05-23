3bf6979ca6617e27b6074b2e662921a632c2fdb3
Miyamizu Mitsuha <41120381+etylix@users.noreply.github.com>
2019-05-23 00:07:10 -0700

    Update aosp_sirius.mk

--- aosp_sirius.mk
+++ aosp_sirius.mk
@@ -7,8 +7,10 @@
 $(call inherit-product, device/xiaomi/sirius/device.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common.mk)
+# Gapps
 TARGET_GAPPS_ARCH := arm64
+# Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_sirius
