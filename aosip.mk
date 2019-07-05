d4b7826ace0e48f0f8bcd7a11a1a5d7cb1ad75a4
Miyamizu Mitsuha <41120381+etylix@users.noreply.github.com>
2019-07-05 14:02:26 +0700

    Update aosip.mk

--- aosip.mk
+++ aosip.mk
@@ -8,6 +8,8 @@ $(call inherit-product, device/xiaomi/sirius/device.mk)
 
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+# PixelStyle
+$(call inherit-product-if-exists, vendor/pixelstyle/config.mk)
 # Gapps
 TARGET_GAPPS_ARCH := arm64
 # Bootanimation
@@ -18,6 +20,7 @@ PRODUCT_DEVICE := sirius
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := LOLIPHONE
 PRODUCT_MANUFACTURER := Xiaomi
+IS_PHONE := true
 
 BUILD_FINGERPRINT := "Xiaomi/sirius/sirius:9/PKQ1.181121.001/V10.3.1.0.PEBCNXM:user/release-keys"
 
