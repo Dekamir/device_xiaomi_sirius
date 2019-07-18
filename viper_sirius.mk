b114f78bb0782714cd843638f142e86b9b729c62
Yunus İşleyen <dekamir@outlook.com>
2019-07-18 17:59:47 +0300

    Initialisation for ViperOS

--- viper_sirius.mk
+++ viper_sirius.mk
@@ -7,13 +7,13 @@
 $(call inherit-product, device/xiaomi/sirius/device.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/havoc/config/common.mk)
+$(call inherit-product, vendor/viper/config/common.mk)
 # Gapps
 TARGET_GAPPS_ARCH := arm64
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_sirius
+PRODUCT_NAME := viper_sirius
 PRODUCT_DEVICE := sirius
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8 SE
